f6f948699d48a685dbf18b250710144c8e80bf93
Gus Caplan <me@gus.host>
2017-10-02 15:20:22 -0500

    speed and drop a dep

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const path = require('path');
-const jsdoc = require('jsdoc-api');
-const logger = require('jsdoc/util/logger');
+const { Doclet } = require('jsdoc/doclet');
 
 const Transforms = {
   lowerCase: (s) => s.toLowerCase(),
@@ -8,12 +7,14 @@ const Transforms = {
 };
 
 const ThisRe = new RegExp(`\\(\\(THIS\\)(${Object.keys(Transforms).join('|')})?\\)`, 'g');
+const DynamicRe = /@dynamic [^\n]+/g;
 
 const addons = [];
 
 exports.defineTags = (dictionary) => {
   dictionary.defineTag('dynamic', {
     onTagged(doclet, tag) {
+      doclet.comment = doclet.comment.replace(DynamicRe, '');
       const ex = require(path.join(doclet.meta.path, doclet.meta.filename));
       const iterator = ex[tag.value.replace('this.', '')];
       for (const item of iterator) {
@@ -24,13 +25,7 @@ exports.defineTags = (dictionary) => {
           const t = modifier ? Transforms[modifier](item) : item;
           comment = comment.replace(ThisRe, t);
         }
-        const [parsed] = jsdoc.explainSync({ source: comment });
-        parsed.meta.path = doclet.meta.path;
-        parsed.meta.filename = doclet.meta.filename;
-        parsed.meta.lineno = doclet.meta.lineno;
-        parsed.meta.columnno = doclet.meta.columno;
-        // logger.warn(`jsdoc-api slowness update: ${item} handled`);
-        addons.push(parsed);
+        addons.push(new Doclet(comment, doclet.meta));
       }
     },
     mustHaveValue: true,
@@ -42,8 +37,8 @@ exports.handlers = {
   parseComplete({ doclets }) {
     for (let i in doclets) {
       const d = doclets[i];
-      if (d.undocumented || !d.comment) continue;
-      if (d.comment.includes('@dynamic')) doclets.splice(i, 1);
+      if (d.undocumented || !d.name) continue;
+      if (ThisRe.test(d.name)) doclets.splice(i, 1);
     }
     doclets.push(...addons);
   },
